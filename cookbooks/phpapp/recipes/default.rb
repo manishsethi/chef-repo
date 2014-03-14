8c84f48eefc527a106aa70147a77d104d76bdac6
manishsethi <manish@sethis.in>
2014-03-14 10:14:45 +0000

    Template is created and wordpress is made.

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -15,7 +15,7 @@ include_recipe "apache2::mod_php5"
 include_recipe "mysql::ruby"
 
 apache_site "default" do
-	enable true
+	enable false
 end
 
 mysql_database node['phpapp']['database'] do
@@ -24,3 +24,64 @@ mysql_database node['phpapp']['database'] do
 	action :create
 end
 
+mysql_database_user node['phpapp']['db_username']do
+	connection({:host => 'localhost', :username =>'root', :password => node['mysql']['server_root_password']}
+)
+	password node['phpapp']['db_password']
+	database_name node['phpapp']['database']
+	privileges [:select, :update, :insert, :create, :delete]
+	action :grant
+end
+
+wordpress_latest = Chef::Config[:file_cache_path] + "/wordpress-latest.tar.gz"
+
+remote_file wordpress_latest do
+  source "http://wordpress.org/latest.tar.gz"
+  mode "0644"
+end
+
+directory node["phpapp"]["path"] do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  recursive true
+end
+
+execute "untar-wordpress" do
+  cwd node['phpapp']['path']
+  command "tar --strip-components 1 -xzf " + wordpress_latest
+  creates node['phpapp']['path'] + "/wp-settings.php"
+end
+
+#creating template
+
+wp_secrets = Chef::Config[:file_cache_path] + '/wp-secrets.php'
+
+if File.exist?(wp_secrets)
+  salt_data = File.read(wp_secrets)
+else
+  require 'open-uri'
+  salt_data = open('https://api.wordpress.org/secret-key/1.1/salt/').read
+  open(wp_secrets, 'wb') do |file|
+    file << salt_data
+  end
+end
+
+template node['phpapp']['path'] + '/wp-config.php' do
+  source 'wp-config.php.erb'
+  mode 0755
+  owner 'root'
+  group 'root' 
+  variables(
+    :database        => node['phpapp']['database'],
+    :user            => node['phpapp']['db_username'],
+    :password        => node['phpapp']['db_password'],
+    :wp_secrets      => salt_data)
+end
+
+web_app 'phpapp' do
+  template 'site.conf.erb'
+  docroot node['phpapp']['path']
+  server_name node['phpapp']['server_name']
+end
